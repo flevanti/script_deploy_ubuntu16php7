ece1931316b607b0f8622dd79327a6b648c15573
flevanti <frenke@frenke.it>
2018-06-23 21:18:45 +0100

    added missing pkgs needed when starting from bare ubuntu instead of AWS ubuntu AMI

--- script.sh
+++ script.sh
@@ -1,10 +1,15 @@
 apt-get update
-apt-get upgrade
-apt-get dist-upgrade
+apt-get upgrade -y
+# apt-get dist-upgrade
+apt-get install software-properties-common
 LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
 LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/apache2
 add-apt-repository 'deb http://archive.ubuntu.com/ubuntu trusty universe'
-apt-get update
+apt-get update -y
+apt-get install dialog -y
+apt-get install openssh-server -y
+apt-get install openssh-client -y
+apt-get install nano -y
 apt-get install bc -y
 apt-get install binutils  -y
 apt-get install fop -y
@@ -18,12 +23,10 @@ apt-get install rsync -y
 apt-get install zip -y
 apt-get install unzip -y
 apt-get install xml2 -y
-
 apt-get install python-software-properties -y
 apt-get install apache2 -y
 apt-get install php7.2 -y
 apt-get install mysql-client-5.6 -y
-
 apt-get install php-bz2 -y
 apt-get install php-curl -y
 apt-get install php-gd -y
@@ -47,20 +50,14 @@ apt-get install php-cli -y
 a2enmod rewrite authz_groupfile headers info
 systemctl restart apache2
 
-# created ssh config
-# imported deploy key
-# imported config files apache
-
-
-# changed www folder owner to ubuntu
-# changed www folder group to www-data
-chown -R ubuntu:www-data www
-
 
 # created catch all vhost + folder
 # created php7 test vhost + folder
 mkdir  public_php7
 mkdir  html_catch_all
+
+cp ./001-public_php7.conf /etc/apache2/sites-available/
+cp ./999-default_catch_all.conf /etc/apache2/sites-available/
 
 # create apache conf file and enable conf
 echo "ServerTokens Prod" | sudo tee  /etc/apache2/conf-available/xxx-servertokens.conf
@@ -69,11 +66,22 @@ a2enconf xxx-servertokens.conf
 echo "ServerSignature Off" | sudo tee  /etc/apache2/conf-available/xxx-serversignature.conf
 a2enconf xxx-serversignature.conf
 
-
-
 # restart apache
 apache2ctl restart
 
 #curl -sS https://getcomposer.org/installer -o composer-setup.php
 #php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 #rm composer-setup.php
+
+
+
+# remember to import deploy private key and create ssh config
+
+
+# change www folder owner to ubuntu
+# change www folder group to www-data
+chown -R ubuntu:www-data www
+
+
+
+
