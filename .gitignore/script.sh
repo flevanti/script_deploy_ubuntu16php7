dec15ab95e2d05fdd9fd95015c080ea95bb9dda3
F. Levanti <francesco@levanti.net>
2018-06-23 20:27:00 +0100

    Create script.sh

---
+++ .gitignore/script.sh
@@ -0,0 +1,79 @@
+apt-get update
+apt-get upgrade
+apt-get dist-upgrade
+LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
+LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/apache2
+add-apt-repository 'deb http://archive.ubuntu.com/ubuntu trusty universe'
+apt-get update
+apt-get install bc -y
+apt-get install binutils  -y
+apt-get install fop -y
+apt-get install fortune -y
+apt-get install memcached -y
+apt-get install run-one -y
+apt-get install screen -y
+apt-get install sed -y
+apt-get install sl -y
+apt-get install rsync -y
+apt-get install zip -y
+apt-get install unzip -y
+apt-get install xml2 -y
+
+apt-get install python-software-properties -y
+apt-get install apache2 -y
+apt-get install php7.2 -y
+apt-get install mysql-client-5.6 -y
+
+apt-get install php-bz2 -y
+apt-get install php-curl -y
+apt-get install php-gd -y
+apt-get install php-http -y
+apt-get install php-igbinary -y
+apt-get install php-mbstring -y
+apt-get install php-mcrypt -y
+apt-get install php-memcached -y
+apt-get install php-msgpack -y
+apt-get install php-mysql -y
+apt-get install php-odbc -y
+apt-get install php-propro -y
+apt-get install php-raphf -y
+apt-get install php-soap -y
+apt-get install php-xdebug -y
+apt-get install php-xml -y
+apt-get install php7.2-xsl -y
+apt-get install php-zip -y
+apt-get install php-cli -y
+
+a2enmod rewrite authz_groupfile headers info
+systemctl restart apache2
+
+# created ssh config
+# imported deploy key
+# imported config files apache
+
+
+# changed www folder owner to ubuntu
+# changed www folder group to www-data
+chown -R ubuntu:www-data www
+
+
+# created catch all vhost + folder
+# created php7 test vhost + folder
+mkdir  public_php7
+mkdir  html_catch_all
+
+# create apache conf file and enable conf
+echo "ServerTokens Prod" | sudo tee  /etc/apache2/conf-available/xxx-servertokens.conf
+a2enconf xxx-servertokens.conf
+
+echo "ServerSignature Off" | sudo tee  /etc/apache2/conf-available/xxx-serversignature.conf
+a2enconf xxx-serversignature.conf
+
+
+
+# restart apache
+apache2ctl restart
+
+#curl -sS https://getcomposer.org/installer -o composer-setup.php
+#php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+#rm composer-setup.php
